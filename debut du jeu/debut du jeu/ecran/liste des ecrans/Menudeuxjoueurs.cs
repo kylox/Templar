17c3104c7ad9e8265c7a3af054fefa4623c7cd7a
Tchi <william.fabre@epita.fr>
2013-04-25 21:58:28 +0200

    caca
    Signed-off-by: Tchi <william.fabre@epita.fr>

---
+++ debut du jeu/debut du jeu/ecran/liste des ecrans/Menudeuxjoueurs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Templar.ecran.liste_des_ecrans
+{
+    class Menudeuxjoueurs : GameScreen
+    {
+        //fields
+        Texture2D image;
+        menugenerale menugenerale;
+        Rectangle rec;
+        public int SelectedIndex
+        {
+            get { return menugenerale.SelectedIndex; }
+            set { menugenerale.SelectedIndex = value; }
+        }
+    }
+}
